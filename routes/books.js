a3f92d1905254b394a224f95dc649832358650f4
greatxrider <daligdig.jephmari@gmail.com>
2024-08-29 21:09:00 +0800

    styling queryDatabase search pagination

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
+const { Op } = require('../models');
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb) {
@@ -16,15 +17,76 @@ function asyncHandler(cb) {
 
 /* GET books listing. */
 router.get('/', asyncHandler(async (req, res) => {
-    const books = await Book.findAll({
-        order: [['createdAt', 'DESC']]
+    const page = parseInt(req.query.page) || 1; // Default to page 1
+    const limit = 2; // Number of items per page
+    const offset = (page - 1) * limit;
+
+    const { rows, count } = await Book.findAndCountAll({
+        order: [['createdAt', 'DESC']],
+        limit,
+        offset
     });
-    res.render('books/index', { books, title: 'Books' });
+
+    const totalPages = Math.ceil(count / limit);
+    const currentPath = req.baseUrl + req.path;
+    res.render('books/index', { rows, title: 'Books', page, totalPages, currentPath });
+}));
+
+/* GET search books */
+router.get('/search', asyncHandler(async (req, res) => {
+    const { query } = req.query;
+    const page = parseInt(req.query.page) || 1; // Default to page 1
+    const limit = 2; // Number of items per page
+    const offset = (page - 1) * limit;
+
+    let books = [];
+    if (query) {
+        try {
+            books = await Book.findAndCountAll({
+                where: {
+                    [Op.or]: [
+                        { title: { [Op.like]: `%${query}%` } },
+                        { author: { [Op.like]: `%${query}%` } },
+                        { genre: { [Op.like]: `%${query}%` } },
+                        { year: { [Op.like]: `%${query}%` } }
+                    ]
+                },
+                order: [['createdAt', 'DESC']],
+                limit,
+                offset
+            });
+
+            if (books.rows.length === 0) {
+                const error = new Error(`No results found for query: "${query}"`);
+                error.status = 404;
+                throw error;
+            }
+        } catch (error) {
+            if (error.name === "SequelizeValidationError") { // checking the error
+                return res.render('books/index', { rows: books.rows, errors: error.errors, title: "Books" });
+            } else if (error.status === 404) {
+                return res.render('books/index', { rows: books.rows, error, title: "Books" });
+            } else {
+                throw error;
+            }
+        }
+    } else {
+        books = await Book.findAndCountAll({
+            order: [['createdAt', 'DESC']],
+            limit,
+            offset
+        });
+    }
+
+    const { rows, count } = books;
+    const totalPages = Math.ceil(count / limit);
+    const currentPath = req.baseUrl + req.path;
+    res.render('books/index', { rows, title: 'Books', page, totalPages, currentPath });
 }));
 
 /* GET shows create new book form */
 router.get('/new', asyncHandler(async (req, res) => {
-    res.render('books/new-book', { book: {}, title: "New Book" });
+    res.render('books/new-book', { book: {}, title: "Books" });
 }));
 
 /* POST create new book */
